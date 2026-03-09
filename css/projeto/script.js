69545f56c50e02bd51928ed6cc8381478f8ca8d8
Your Name <you@example.com>
2026-03-09 15:19:19 -0300

    mudei algumas coisas

--- css/projeto/script.js
+++ css/projeto/script.js
@@ -59,3 +59,43 @@ function areaCirculo() {
     document.getElementById('resultado-triangulo').innerText = resultado;
 }
 
+function areaSetorCircular() {
+    let num1 = parseFloat(document.getElementById('1').value); // Ângulo (α)
+    let num2 = parseFloat(document.getElementById('2').value); // Raio (r)
+    let resultado = (num1 / 360) * 3.14 * (num2 ** 2);
+    document.getElementById('resultado-triangulo').innerText = resultado;
+}
+
+function areaCoroaCircular() {
+    let num1 = parseFloat(document.getElementById('1').value); // Raio maior (R)
+    let num2 = parseFloat(document.getElementById('2').value); // Raio menor interno (r)
+    let resultado = 3.14 * ((num1 ** 2) - (num2 ** 2));
+    document.getElementById('resultado-triangulo').innerText = resultado;
+}
+
+function areaElipse() {
+    let num1 = parseFloat(document.getElementById('1').value); // Semieixo maior (a)
+    let num2 = parseFloat(document.getElementById('2').value); // Semieixo menor (b)
+    let resultado = 3.14 * num1 * num2;
+    document.getElementById('resultado-triangulo').innerText = resultado;
+}
+
+function areaParalelogramo() {
+    let num1 = parseFloat(document.getElementById('1').value); // Base (b)
+    let num2 = parseFloat(document.getElementById('2').value); // Altura perpendicular (h)
+    let resultado = num1 * num2;
+    document.getElementById('resultado-triangulo').innerText = resultado;
+}
+
+function areaPoligonoRegular() {
+    let num1 = parseFloat(document.getElementById('1').value); // Semiperímetro (p)
+    let num2 = parseFloat(document.getElementById('2').value); // Apótema (a)
+    let resultado = num1 * num2;
+    document.getElementById('resultado-triangulo').innerText = resultado;
+}
+
+function areaCirculo() {
+    let num1 = parseFloat(document.getElementById('1').value);
+    let resultado = 3.14 * (num1 ** 2);
+    document.getElementById('resultado-triangulo').innerText = resultado;
+}
